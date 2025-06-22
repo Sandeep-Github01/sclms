1aa38a2c5870fd0c818673bc72e692f704241980
Your_Name <Your_Email>
2025-06-22 17:10:02 +0545

    changes

--- resources/views/frontend/user/register.blade.php
+++ resources/views/frontend/user/register.blade.php
@@ -1,8 +1,17 @@
-<div style="width: 400px; margin: 50px auto;">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+</head>
+<body>
+
+<div class="auth-container">
     <h2>Register New Account</h2>
 
     @if($errors->any())
-        <div style="color: red;">
+        <div class="alert">
             @foreach($errors->all() as $error)
                 <p>{{ $error }}</p>
             @endforeach
@@ -12,29 +21,34 @@
     <form action="{{ route('frontend.user.registerSave') }}" method="POST">
         @csrf
 
-        <label>Name:</label><br>
-        <input type="text" name="name" required><br><br>
+        <label>Name:</label>
+        <input type="text" name="name" required>
 
-        <label>Email:</label><br>
-        <input type="email" name="email" required><br><br>
+        <label>Email:</label>
+        <input type="email" name="email" required>
 
-        <label>Password:</label><br>
-        <input type="password" name="password" required><br><br>
+        <label>Password:</label>
+        <input type="password" name="password" required>
+        <i class="far fa-eye toggle-password" id="togglePassword"></i>
 
-        <label>Confirm Password:</label><br>
-        <input type="password" name="password_confirmation" required><br><br>
+        <label>Confirm Password:</label>
+        <input type="password" name="password_confirmation" required>
+        <i class="far fa-eye toggle-password" id="togglePassword"></i>
 
-        <label>Role:</label><br>
+        <label>Role:</label>
         <select name="role" required>
             <option value="student">Student</option>
             <option value="teacher">Teacher</option>
-        </select><br><br>
+        </select>
 
-        <label>Department ID:</label><br>
-        <input type="number" name="department_id" required><br><br>
+        <label>Department ID:</label>
+        <input type="number" name="department_id" required>
 
         <button type="submit">Register</button>
     </form>
 
-    <p>Already registered? <a href="{{ route('frontend.user.login') }}">Login here</a></p>
+    <p style="text-align:center;">Already registered? <a href="{{ route('frontend.user.login') }}">Login here</a></p>
 </div>
+
+</body>
+</html>
